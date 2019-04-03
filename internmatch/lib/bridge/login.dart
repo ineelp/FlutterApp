34619f4ff852a9219ab5b01383700f30499b2db6
baradghimire <baradghimire@gmail.com>
2019-04-04 10:49:45 +1100

    new Barad Branch

--- internmatch/lib/bridge/login.dart
+++ internmatch/lib/bridge/login.dart
@@ -2,35 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
-
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class Login extends StatelessWidget {
-String realm="";
+/* String realm="";
 String auth_server_url="";
 String credentials="";
 String vertx_url="";
-String api_url="";
+String api_url=""; */
 
   Map _data;
   final TextEditingController _userController = new TextEditingController();
   final TextEditingController _passwordController = new TextEditingController();
   String _welcomeString = "";
 
-  void _erase() {
-    _userController.clear();
-    _passwordController.clear();
+  void _showWelcome() async {
+    _data = await getBridge();
+
+    print(_data);
   }
 
-
-  void _showWelcome() async{
-
-      _data = await getBridge();
-
-
-
-
-
+void _opennewpage(BuildContext context) {
+    Navigator.of(context).pushNamed('/widget');
   }
+
+  
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -45,7 +42,6 @@ String api_url="";
           alignment: Alignment.topCenter,
           child: new ListView(
             children: <Widget>[
-
               new Padding(padding: const EdgeInsets.all(50.6)),
               //form
               new Container(
@@ -54,9 +50,7 @@ String api_url="";
                 color: Colors.white,
                 child: new Column(
                   children: <Widget>[
-                    new Image.asset('images/logo.png',
-                        width: 250,
-                        height: 120),
+                    new Image.asset('images/logo.png', width: 250, height: 120),
                     new TextField(
                         controller: _userController,
                         decoration: new InputDecoration(
@@ -70,7 +64,6 @@ String api_url="";
                         icon: new Icon(Icons.lock),
                       ),
                       obscureText: true,
-
                     ),
 
                     //add padding
@@ -82,7 +75,10 @@ String api_url="";
                           new Container(
                             margin: const EdgeInsets.only(left: 150.0),
                             child: new RaisedButton(
-                                onPressed:  _showWelcome,
+//                                onPressed: _showWelcome,
+                                onPressed: () {
+                                  _opennewpage();
+                                },
                                 color: Colors.blueGrey[700],
                                 child: new Text("Login",
                                     style: new TextStyle(
@@ -95,31 +91,31 @@ String api_url="";
                     ),
                   ],
                 ),
-              ) ,//forms ends here
+              ), //forms ends here
               new Padding(padding: const EdgeInsets.all(10.5)),
               new Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  new Text("Welcome, $_welcomeString",
+                  new Text(
+                    "Welcome, $_welcomeString",
                     style: new TextStyle(
                         color: Colors.white,
                         fontSize: 19.4,
-                        fontWeight: FontWeight.w500),),
-
-
+                        fontWeight: FontWeight.w500),
+                  ),
                 ],
               )
             ],
           ),
         ));
   }
-  Future<Map> getBridge() async{
 
-    String apiUrl = "https://internmatch.outcome-hub.com/api/events/init?url=https://internmatch.outcome-hub.com";
+  Future<Map> getBridge() async {
+    String apiUrl =
+        "https://internmatch.outcome-hub.com/api/events/init?url=https://internmatch.outcome-hub.com";
 
-    http.Response response= await http.get(apiUrl);
+    http.Response response = await http.get(apiUrl);
 
     return json.decode(response.body);
-
   }
 }
